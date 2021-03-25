f4cf6b3066f17ac832415af91a0c05a220bb215c
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-03-25 08:39:30 -0300

    Primeiro batch, em um simples hello world.

---
+++ PrimeiroBatch/src/main/java/com/udemy/PrimeiroBatch/BatchConfig.java
@@ -0,0 +1,43 @@
+package com.udemy.PrimeiroBatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job printOlaMundo() {
+        return jobBuilderFactory.get("printOlaMundo").start(imprimeOlaMundo()).build();
+    }
+
+    private Step imprimeOlaMundo() {
+        return stepBuilderFactory.get("printOlaMundoStep").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.print("Olá mundo ");
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+
+    }
+
+
+}
