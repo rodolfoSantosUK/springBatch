86d9c7585e5f1841b44f7e6073cf13a575e569cf
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-03-27 08:04:38 -0300

     Externalizando arquivo de properties

---
+++ PrimeiroBatch/src/main/java/com/udemy/PrimeiroBatch/config/PropsConfig.java
@@ -0,0 +1,19 @@
+package com.udemy.PrimeiroBatch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class PropsConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer config() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource("/etc/config/springBatch/application.properties"));
+        return configurer;
+    }
+
+
+}
