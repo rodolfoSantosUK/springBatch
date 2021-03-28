91c38e7286f62e8b171d6cd97260629653ff01ce
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-03-28 15:35:50 -0300

    Processador Composto

--- ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
+++ ProcessadorValidacaoJob/src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -1,6 +1,8 @@
 package com.springbatch.processadorvalidacao.processor;
 
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.CompositeItemProcessor;
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
 import org.springframework.batch.item.validator.BeanValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidatingItemProcessor;
 import org.springframework.batch.item.validator.ValidationException;
@@ -18,11 +20,25 @@ public class ProcessadorValidacaoProcessorConfig {
     private Set<String> emails = new HashSet<String>();
 
     @Bean
-    public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() {
+    public ItemProcessor<Cliente, Cliente> procesadorValidacaoProcessor() throws Exception {
+        return new CompositeItemProcessorBuilder<Cliente, Cliente>()
+                .delegates(beanValidatingItemProcessor(), emailValidationProcessor())
+                .build();
+    }
+
+    private BeanValidatingItemProcessor<Cliente> beanValidatingItemProcessor() throws Exception {
+        BeanValidatingItemProcessor<Cliente> processor = new BeanValidatingItemProcessor();
+        processor.setFilter(true);
+        processor.afterPropertiesSet();
+        return processor;
+    }
+
+    private ValidatingItemProcessor emailValidationProcessor() {
         ValidatingItemProcessor<Cliente> processor = new ValidatingItemProcessor<>();
         processor.setValidator(validator());
         processor.setFilter(true);
         return processor;
+
     }
 
     private Validator<? super Cliente> validator() {
